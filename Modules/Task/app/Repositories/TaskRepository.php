a11d0ce7b202d02e7ec83ac1712937368cb6b062
To Huu <buitohuu123@gmail.com>
2025-08-17 17:21:10 +0700

    DoiThay

--- Modules/Task/app/Repositories/TaskRepository.php
+++ Modules/Task/app/Repositories/TaskRepository.php
@@ -2,7 +2,15 @@
 
 namespace Modules\Task\Repositories;
 
+use Modules\Task\app\Models\Task;
+
 class TaskRepository
 {
     public function handle() {}
+
+
+    //METHOD INDEX
+    public function filter(array $filter){
+        $query = Task::query();
+    }
 }
